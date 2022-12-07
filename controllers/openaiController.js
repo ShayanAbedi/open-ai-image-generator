6358471a860d01c5c2e4954ad4c1103bb287e3e1
Shayan <shayanabedi.dev@gmail.com>
2022-12-07 17:20:16 -0500

    initial commit

---
+++ controllers/openaiController.js
@@ -0,0 +1,38 @@
+const { Configuration, OpenAIApi } = require('openai');
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+const openai = new OpenAIApi(configuration);
+
+const generateImage = async (req, res) => {
+  const { prompt, size, n } = req.body;
+
+  const imageSize =
+    size === 'small' ? '256x256' : size === 'medium' ? '512x512' : '1024x1024';
+
+  try {
+    const response = await openai.createImage({
+      prompt,
+      n: +n,
+      size: imageSize,
+    });
+    const imageUrl = response.data.data;
+    res.status(200).json({
+      success: true,
+      data: imageUrl,
+    });
+  } catch (error) {
+    res.status(400).json({
+      success: false,
+      error: 'The image could not be generated',
+    });
+    if (error.response) {
+      console.log(error.response.status);
+      console.log(error.response.data);
+    } else {
+      console.log(error.message);
+    }
+  }
+};
+
+module.exports = { generateImage };
